70acad6f3399401828309d54a9aad1a18daffb21
Sardor Xalilov <138990743+sardorxx@users.noreply.github.com>
2024-03-12 18:24:05 +0500

    Delete features directory

--- features/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from features.models import QuestionAnswer
-
-
-class QuestionAnswerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'question')
-
-
-admin.site.register(QuestionAnswer)
--- features/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from features.views import answer_question, news_list
-
-app_name = 'features'
-urlpatterns = [
-    path('question-answers/', answer_question, name='answer_question'),
-    path('news_list/', news_list, name='news_list')
-]
